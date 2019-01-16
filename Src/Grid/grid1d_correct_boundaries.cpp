f3364b084882dd6ee3949ea325008c801bbf3014
Niceno <bojan.niceno.scientist@gmail.com>
2019-01-16 19:14:26 +0100

    Works with expanded buffers.

--- Src/Grid/grid1d_correct_boundaries.cpp
+++ Src/Grid/grid1d_correct_boundaries.cpp
@@ -3,26 +3,31 @@
 /******************************************************************************/
 void Grid1D::correct_boundaries() {
 
-  const real D1 = x_node[2]   - x_node[1];
-  const real DN = x_node[N+1] - x_node[N];
-
   /*------------------------+
   |                         |
   |  handle boundary nodes  |
   |                         |
   +------------------------*/
-  x_node[0]   = x_node[1];
-  x_node[N+2] = x_node[N+1];
-  if(periodic1()==true) x_node[0]   -= D1;
-  if(periodicN()==true) x_node[N+2] += DN;
+  for(int b=0; b<boil::BW; b++) {
+    x_node[b]                        = x_node[boil::BW];
+    x_node[nc_in + boil::BW + b + 1] = x_node[nc_in + boil::BW];
+  }
+
+  for(int b=0; b<boil::BW; b++) {
+    real dx1 = x_node[boil::BW + b + 1] - x_node[boil::BW];
+    if(periodicN() == true) x_node[nc_in + boil::BW + b + 1] += dx1;
+
+    real dxN = x_node[nc_in + boil::BW] - x_node[nc_in + boil::BW - b - 1];
+    if(periodic1() == true) x_node[boil::BW - b - 1] -= dxN;
+  }
 
   /*--------------------------+            N = 4
   |                           |
   |  create cell coordinates  |    0   1   2   3   4   5
   |      and cell sizes       |  | o |-O-+-O-+-O-+-O-| o |
-  |                           |  0   1   2   3   4   5   6    
-  +--------------------------*/                                
-  for(int i=0; i<N+2; i++) {
+  |                           |  0   1   2   3   4   5   6
+  +--------------------------*/
+  for(int i=0; i<nc_in + 2*boil::BW; i++) {
      x_cell[i] = 0.5 * (x_node[i+1] + x_node[i]);
     dx_cell[i] =       (x_node[i+1] - x_node[i]);
   }
@@ -32,11 +37,13 @@ void Grid1D::correct_boundaries() {
   |  create node (staggered cells) sizes  |    0   1   2   3   4   5
   |                                       |  | o |-O-+-O-+-O-+-O-| o |
   +---------------------------------------+  0   1   2   3   4   5   6  */
-  for(int i=1; i<N+2; i++) 
+  for(int i=1; i<nc_in + 2*boil::BW; i++) 
     dx_node[i] = x_cell[i] - x_cell[i-1];
 
-  dx_node[0]   = 0;
-  dx_node[N+2] = 0;
-  if(periodic1()==true) dx_node[0]   = x_cell[N] - x_cell[N-1];
-  if(periodicN()==true) dx_node[N+2] = x_cell[2] - x_cell[1];
+  dx_node[0]                  = 0;
+  dx_node[nc_in + 2*boil::BW] = 0;
+  if(periodic1()==true) dx_node[0]   = x_cell[nc_in+2*boil::BW] 
+                                     - x_cell[nc_in+2*boil::BW-1];
+  if(periodicN()==true) dx_node[nc_in+2*boil::BW] = x_cell[boil::BW+1] 
+                                                  - x_cell[boil::BW];
 }
