bfec9e390170c770a71d6b9b7cb3aaa7da25ff94
Niceno <bojan.niceno.scientist@gmail.com>
2019-01-16 19:12:52 +0100

    Works with extended buffers! Also simplified the code at some places.

--- Src/Solver/Additive/additive_restriction.cpp
+++ Src/Solver/Additive/additive_restriction.cpp
@@ -30,33 +30,36 @@ void AC::restriction(const Centered & h, Centered & H) const {
 |                                                                  |
 +-----------------------------------------------------------------*/
 
-  const int CI( (h.ni()-2) / (H.ni()-2) );
-  const int CJ( (h.nj()-2) / (H.nj()-2) );
-  const int CK( (h.nk()-2) / (H.nk()-2) );
-  int ih_[CI+2], jh_[CJ+2], kh_[CK+2];
-  int ih [CI+2], jh [CJ+2], kh [CK+2];
+  const int CI( (h.ei()-h.si()+1) / (H.ei()-H.si()+1) );
+  const int CJ( (h.ej()-h.sj()+1) / (H.ej()-H.sj()+1) );
+  const int CK( (h.ek()-h.sk()+1) / (H.ek()-H.sk()+1) );
+
+  int ih[CI+2], jh[CJ+2], kh[CK+2];
 
   real D[CI+1][CJ+1][CK+1]; // [0] not used
-	
+
   h.phi.exchange();
 
-  for(int iH=1; iH < H.ni()-1; iH++) {
-        /* create indexes */
-        ih_[CI]   = iH * CI;
-        ih_[CI+1] = ih_[CI] + 1;
-        for(int i=1; i<=CI; i++) ih_[CI-i] = ih_[CI]-i;
+  for(int iH=H.si(); iH<=H.ei(); iH++) {
 
-    for(int jH=1; jH < H.nj()-1; jH++) {
-          /* create indexes */
-          jh_[CJ]   = jH * CJ;
-          jh_[CJ+1] = jh_[CJ] + 1;
-          for(int j=1; j<=CJ; j++) jh_[CJ-j] = jh_[CJ]-j;
-    
-      for(int kH=1; kH < H.nk()-1; kH++) {
-            /* create indexes */
-            kh_[CK]   = kH * CK;
-            kh_[CK+1] = kh_[CK] + 1;
-            for(int k=1; k<=CK; k++) kh_[CK-k] = kh_[CK]-k;
+    /* create indexes */
+    ih[CI]   = iH*CI - (H.si() - 1) * (CI - 1);
+    ih[CI+1] = ih[CI] + 1;
+    for(int i=1; i<=CI; i++) ih[CI-i] = ih[CI]-i;
+
+    for(int jH=H.sj(); jH<=H.ej(); jH++) {
+
+      /* create indexes */
+      jh[CJ]   = jH*CJ - (H.sj() - 1) * (CJ - 1);
+      jh[CJ+1] = jh[CJ] + 1;
+      for(int j=1; j<=CJ; j++) jh[CJ-j] = jh[CJ]-j;
+
+      for(int kH=H.sk(); kH<=H.ek(); kH++) {
+
+        /* create indexes */
+        kh[CK]   = kH*CK - (H.sk() - 1) * (CK - 1);
+        kh[CK+1] = kh[CK] + 1;
+        for(int k=1; k<=CK; k++) kh[CK-k] = kh[CK]-k;
 
         /*-----------------------------------------------------------------+ 
         |  compute new diagonal terms and start computing right hand side  |
@@ -65,72 +68,59 @@ void AC::restriction(const Centered & h, Centered & H) const {
         for(int i=1; i<=CI; i++)
           for(int j=1; j<=CJ; j++)
             for(int k=1; k<=CK; k++) {
-              D[i][j][k]          = h.A.c [ih_[i]][jh_[j]][kh_[k]];
-              H.fnew[iH][jH][kH] += h.fnew[ih_[i]][jh_[j]][kh_[k]];
+              D[i][j][k]          = h.A.c [ih[i]][jh[j]][kh[k]];
+              H.fnew[iH][jH][kH] += h.fnew[ih[i]][jh[j]][kh[k]];
             }
-
-        /* update indexes */
-        for(int i=1; i<=CI; i++) ih[i] = ih_[i];
-        for(int j=1; j<=CJ; j++) jh[j] = jh_[j];
-        for(int k=1; k<=CK; k++) kh[k] = kh_[k];
 
         /*------+
         |  e-w  |
         +------*/
         for(int j=1; j<=CJ; j++)
           for(int k=1; k<=CK; k++) {
-            H.fnew[iH][jH][kH] += h.A.e[ih_[CI  ]][jh [j]][kh [k]] 
-                                * h.phi[ih_[CI+1]][jh_[j]][kh_[k]];
-            H.fnew[iH][jH][kH] += h.A.w[ih_[1   ]][jh [j]][kh [k]] 
-                                * h.phi[ih_[0   ]][jh_[j]][kh_[k]];
+            H.fnew[iH][jH][kH] += h.A.e[ih[CI  ]][jh[j]][kh[k]] 
+                                * h.phi[ih[CI+1]][jh[j]][kh[k]];
+            H.fnew[iH][jH][kH] += h.A.w[ih[1   ]][jh[j]][kh[k]] 
+                                * h.phi[ih[0   ]][jh[j]][kh[k]];
 
             for(int i=1; i< CI; i++)
-              D[i][j][k] -= (h.A.e[ih_[i]][jh[j]][kh[k]]);
+              D[i][j][k] -= (h.A.e[ih[i]][jh[j]][kh[k]]);
 
             for(int i=CI; i>1; i--)
-              D[i][j][k] -= (h.A.w[ih_[i]][jh[j]][kh[k]]);
+              D[i][j][k] -= (h.A.w[ih[i]][jh[j]][kh[k]]);
           }
-
-        /* update indexes */
-        for(int j=1; j<=CJ; j++) jh[j] = jh_[j];
-        for(int k=1; k<=CK; k++) kh[k] = kh_[k];
 
         /*------+
         |  n-s  |
         +------*/
         for(int i=1; i<=CI; i++)
           for(int k=1; k<=CK; k++) {
-            H.fnew[iH][jH][kH] += h.A.n[ih [i]][jh_[CJ  ]][kh [k]] 
-                                * h.phi[ih_[i]][jh_[CJ+1]][kh_[k]];
-            H.fnew[iH][jH][kH] += h.A.s[ih [i]][jh_[1   ]][kh [k]] 
-                                * h.phi[ih_[i]][jh_[0   ]][kh_[k]];
+            H.fnew[iH][jH][kH] += h.A.n[ih[i]][jh[CJ  ]][kh[k]] 
+                                * h.phi[ih[i]][jh[CJ+1]][kh[k]];
+            H.fnew[iH][jH][kH] += h.A.s[ih[i]][jh[1   ]][kh[k]] 
+                                * h.phi[ih[i]][jh[0   ]][kh[k]];
 
             for(int j=1; j< CJ; j++)
-              D[i][j][k] -= (h.A.n[ih[i]][jh_[j]][kh[k]]);
+              D[i][j][k] -= (h.A.n[ih[i]][jh[j]][kh[k]]);
 
             for(int j=CJ; j>1; j--)
-              D[i][j][k] -= (h.A.s[ih[i]][jh_[j]][kh[k]]);
+              D[i][j][k] -= (h.A.s[ih[i]][jh[j]][kh[k]]);
           }
-
-        /* update indexes */
-        for(int i=1; i<=CI; i++) ih[i] = ih_[i];
-        for(int k=1; k<=CK; k++) kh[k] = kh_[k];
 
         /*------+
         |  t-b  |
         +------*/
         for(int i=1; i<=CI; i++)
           for(int j=1; j<=CJ; j++) {
-            H.fnew[iH][jH][kH] += h.A.t[ih [i]][jh [j]][kh_[CK  ]]
-                                * h.phi[ih_[i]][jh_[j]][kh_[CK+1]];
-            H.fnew[iH][jH][kH] += h.A.b[ih [i]][jh [j]][kh_[1   ]]
-                                * h.phi[ih_[i]][jh_[j]][kh_[0   ]];
+            H.fnew[iH][jH][kH] += h.A.t[ih[i]][jh[j]][kh[CK  ]]
+                                * h.phi[ih[i]][jh[j]][kh[CK+1]];
+            H.fnew[iH][jH][kH] += h.A.b[ih[i]][jh[j]][kh[1   ]]
+                                * h.phi[ih[i]][jh[j]][kh[0   ]];
 
             for(int k=1; k< CK; k++)
-              D[i][j][k] -= h.A.t[ih[i]][jh[j]][kh_[k]];
+              D[i][j][k] -= h.A.t[ih[i]][jh[j]][kh[k]];
 
             for(int k=CK; k> 1; k--)
-              D[i][j][k] -= h.A.b[ih[i]][jh[j]][kh_[k]];
+              D[i][j][k] -= h.A.b[ih[i]][jh[j]][kh[k]];
           }
 
         /*---------------------------+ 
@@ -139,10 +129,10 @@ void AC::restriction(const Centered & h, Centered & H) const {
         for(int i=1; i<=CI; i++)
           for(int j=1; j<=CJ; j++) 
             for(int k=1; k<=CK; k++) 
-              H.fnew[iH][jH][kH] -= D[i][j][k] * h.phi[ih_[i]][jh_[j]][kh_[k]];
-      }	
-    }  
-  }	  
+              H.fnew[iH][jH][kH] -= D[i][j][k] * h.phi[ih[i]][jh[j]][kh[k]];
+      }
+    }
+  }
 
   /*----------------------------------------------------+ 
   |  r.h.s. normalization.                              |
@@ -152,20 +142,21 @@ void AC::restriction(const Centered & h, Centered & H) const {
   |  get distorted r.h.s. on even coarser levels        |
   +----------------------------------------------------*/
   real sum = 0.0;
-  for(int i=1; i<=H.ni()-2; i++)
-    for(int j=1; j<=H.nj()-2; j++)
-      for(int k=1; k<=H.nk()-2; k++) 
+  for(int i=H.si(); i<=H.ei(); i++)
+    for(int j=H.sj(); j<=H.ej(); j++)
+      for(int k=H.sk(); k<=H.ek(); k++)
         sum += H.fnew[i][j][k];
 
   boil::cart.sum_real(& sum);
 
-  int ncell = (H.ni()-2)*(H.nj()-2)*(H.nk()-2);
+  int ncell = (H.ni()-2*boil::BW)*(H.nj()-2*boil::BW)*(H.nk()-2*boil::BW);
   boil::cart.sum_int( & ncell );
 
   sum /= (real)ncell;
 
-  for(int i=1; i<=H.ni()-2; i++)
-    for(int j=1; j<=H.nj()-2; j++)
-      for(int k=1; k<=H.nk()-2; k++) 
+  for(int i=H.si(); i<=H.ei(); i++)
+    for(int j=H.sj(); j<=H.ej(); j++)
+      for(int k=H.sk(); k<=H.ek(); k++)
         H.fnew[i][j][k] -= sum;
+
 }
