80e750b727d91accbaaaf9e7e7e44e4789587670
bures_l <lubomir.bures@psi.ch>
2020-04-09 13:14:58 +0200

    Added linear blending in second layer for near-wall modelling

--- Src/Equation/Centered/PhaseChangeVOF/phasechangevof_near_wall_model.cpp
+++ Src/Equation/Centered/PhaseChangeVOF/phasechangevof_near_wall_model.cpp
@@ -23,11 +23,10 @@ void PhaseChangeVOF::near_wall_model(const Scalar * diff_eddy) {
   
   /* only if heat transfer occurs in the solid */
   if(solid()) {
+    /* z direction */
+    Comp m = Comp::w();
+
     for_ijk(i,j,k) {
-
-      /* z direction */
-      Comp m = Comp::w();
-
       /* bottom is in wall and this is an interfacial cell */
       if(dom->ibody().off(i,j,k-1) && Interface(i,j,k)) {
         real lv = lambdav;
@@ -64,8 +63,61 @@ void PhaseChangeVOF::near_wall_model(const Scalar * diff_eddy) {
         /* we assume only liquid plays a role and is 'between' solid and gas */
         tnl[i][j][k] = lam_s/ll*(tmp_w-tmp_s)/len_s * (-1);
         tnv[i][j][k] = 0.0;
-      }
+      } /* 1 above solid */
     } /* ijk */
+#if 1
+    tnl.exchange();
+    tnv.exchange();
+
+    /* further stabilisation through blending in the second layer */  
+    for_ijk(i,j,k) {
+      if(dom->ibody().off(i,j,k-2) && Interface(i,j,k)) {
+        /* linear blending based on film height is used */
+        real blendfactor = std::min(1.0,std::max(0.0,clr[i][j][k-1]))
+                          +std::min(1.0,std::max(0.0,clr[i][j][k  ]))-1.;
+        blendfactor = std::min(1.0,std::max(0.0,blendfactor));
+
+        real tnlnew, tnvnew;
+        /* inherit heat flux from a interfacial cell adjacent to solid */
+        if(dom->ibody().on(i,j,k-1) && Interface(i,j,k-1)) {
+          tnlnew = tnl[i][j][k-1];
+          tnvnew = tnv[i][j][k-1];
+        } else {
+          real lv = lambdav;
+          real ll = lambdal;
+
+          if (diff_eddy) {
+            lv += (*diff_eddy)[i][j][k]*cpv/rhov/turbP;
+            ll += (*diff_eddy)[i][j][k]*cpl/rhol/turbP;
+          }
+
+          real tmp_w = bndtpr[m][i][j][k-1];
+          real len_s = phi.dzb(k-1) - 0.5*phi.dzc(k-1);
+          real lam_s = solid()->lambda(i,j,k-2);
+          real tmp_s = tpr[i][j][k-2]; 
+
+          /* interface exists between the solid and cell centre */
+          /* this should never occur! */
+          if(Interface(-1,m,i,j,k-1)) {
+            tnlnew = tnl[i][j][k];
+            tnvnew = tnv[i][j][k];
+          } else {
+            assert(boil::realistic(tmp_w));
+            tnlnew = lam_s/ll*(tmp_w-tmp_s)/len_s * (-1);
+            tnvnew = 0.0;
+          }
+        }
+
+        tnl[i][j][k] *= blendfactor;
+        tnv[i][j][k] *= blendfactor;
+
+        tnl[i][j][k] += (1.-blendfactor)*tnlnew;
+        tnv[i][j][k] += (1.-blendfactor)*tnvnew;
+      } /* 2 above solid */
+    } /* ijk */
+#endif
+    tnl.exchange();
+    tnv.exchange();
   } /* solid */
 
   return;
