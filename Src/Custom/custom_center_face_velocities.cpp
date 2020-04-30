3b8a66501c4dd9a789040039d9361fd0238a8866
bures_l <lubomir.bures@psi.ch>
2020-04-30 17:09:42 +0200

    Continued domain-extension algorithm development

--- Src/Custom/custom_center_face_velocities.cpp
+++ Src/Custom/custom_center_face_velocities.cpp
@@ -24,14 +24,20 @@ namespace boil {
   *******************************************************************************/
     Comp m = Comp::u();
     for_vmijk(uvw,m,i,j,k) {
+      if(u.domain()->ibody().off(i,j,k)||u.domain()->ibody().off(i-1,j,k))
+        continue;
       uvw[m][i][j][k] = 0.5*(u[i][j][k]+u[i-1][j][k]);
     }
     m = Comp::v();
     for_vmijk(uvw,m,i,j,k) {
+      if(v.domain()->ibody().off(i,j,k)||v.domain()->ibody().off(i,j-1,k))
+        continue;
       uvw[m][i][j][k] = 0.5*(v[i][j][k]+v[i][j-1][k]);
     }
     m = Comp::w();
     for_vmijk(uvw,m,i,j,k) {
+      if(w.domain()->ibody().off(i,j,k)||w.domain()->ibody().off(i,j,k-1))
+        continue;
       uvw[m][i][j][k] = 0.5*(w[i][j][k]+w[i][j][k-1]);
     }
 
