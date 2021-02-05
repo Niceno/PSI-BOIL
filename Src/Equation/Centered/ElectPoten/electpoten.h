0d6fbe058391a1b4184e432036362280fc492cee
sato <yohei.sato@psi.ch>
2021-02-05 16:25:43 +0100

    bug fixed.

--- Src/Equation/Centered/ElectPoten/electpoten.h
+++ Src/Equation/Centered/ElectPoten/electpoten.h
@@ -64,7 +64,7 @@ class ElectPoten : public Centered {
     void update_vel();
 
     // Calculate Lorenz force
-    void ElectPoten::force_lorenz(Vector * v);
+    void force_lorenz(Vector * v);
 
   protected:
      
