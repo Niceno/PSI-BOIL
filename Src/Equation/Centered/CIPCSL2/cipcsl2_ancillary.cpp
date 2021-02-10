ceacc61e034595258a64bc8562e3fb157fc5c586
sato <yohei.sato@psi.ch>
2021-02-10 18:26:55 +0100

    add DEBUG

--- Src/Equation/Centered/CIPCSL2/cipcsl2_ancillary.cpp
+++ Src/Equation/Centered/CIPCSL2/cipcsl2_ancillary.cpp
@@ -1,4 +1,5 @@
 #include "cipcsl2.h"
+//#define DEBUG
 
 /******************************************************************************/
 void CIPCSL2::ancillary() {
@@ -10,14 +11,26 @@ void CIPCSL2::ancillary() {
 
   /* wall values extrapolation */
   update_at_walls(phi);
+#ifdef DEBUG
+  std::cout<<"ancillary:update_at_walls\n";
+#endif
 
   /* calculate free surface position,
      no subgrid interfaces near walls! */
   heavi->cal_fs_interp(phi,fs,0.0,false);
+#ifdef DEBUG
+  std::cout<<"ancillary:heavi\n";
+#endif
 
   /* normal vector */
   distfunc(phi,24); 
+#ifdef DEBUG
+  std::cout<<"ancillary:distfunc\n";
+#endif
   gradphic(dist);
+#ifdef DEBUG
+  std::cout<<"ancillary:gradphic\n";
+#endif
 
   /* calculate area density */
   heavi->calculate_adens();
