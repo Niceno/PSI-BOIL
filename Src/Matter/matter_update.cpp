3f8bf8a8a7428990bd03c6474c2dbd0b1f5fee65
sato <yohei.sato@psi.ch>
2021-03-31 11:45:42 +0200

    add bracket to avoid warning message of cray compiler

--- Src/Matter/matter_update.cpp
+++ Src/Matter/matter_update.cpp
@@ -15,18 +15,20 @@ void Matter::look_up(const Set & s, const Scalar & sca,
   if(s == Set::beta  ()) texp->look_up(sca,tab,col0,col1);
   if(s == Set::mmass ()) molm->look_up(sca,tab,col0,col1);
   if(s == Set::sigma_e()) sige->look_up(sca,tab,col0,col1);
-  if(s == Set::sigma ()) 
+  if(s == Set::sigma ()) {
     if(tens == NULL) {
       boil::oout << "# Fatal: using surface tension makes ";
       boil::oout << "sense only for mixtures. Exiting!"; 
       boil::oout << boil::endl;    
     } else
       tens->look_up(sca,tab,col0,col1);
-  if(s == Set::latent ())
+  }
+  if(s == Set::latent ()) {
     if(heat == NULL) {
       boil::oout << "# Fatal: using latent heat makes ";
       boil::oout << "sense only for mixtures. Exiting!";
       boil::oout << boil::endl;
     } else
       heat->look_up(sca,tab,col0,col1);
+  }
 }
