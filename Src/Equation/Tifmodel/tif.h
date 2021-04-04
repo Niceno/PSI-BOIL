babaf204dda6389cad67283dada43565f9bc6873
sato <yohei.sato@psi.ch>
2021-04-04 13:58:32 +0200

    synchronize with cylindrical

---
+++ Src/Equation/Tifmodel/tif.h
@@ -0,0 +1,83 @@
+#ifndef TIF_H
+#define TIF_H
+
+#include "../../Field/Scalar/scalar.h"
+#include "../../Field/ScalarInt/scalarint.h"
+#include "../Topology/topology.h"
+#include "../../Matter/matter.h"
+#include "../../Global/global_realistic.h"
+#include "../../Plot/plot.h"
+
+///////////////////////////////////
+//                               //
+//  Interface temperature model  //
+//                               //
+///////////////////////////////////
+class TIF {
+  public:
+    TIF(const real tref); 
+    TIF(const real tref, Topology * topo); 
+    ~TIF() {}
+
+    void init() {
+      if(variable_tif) { 
+        model(); 
+        tif.bnd_update();
+        tif.exchange_all();
+        extend_tint();
+      }
+    }
+
+    void update_tifold() {
+      tifold = tif;
+      store_tif = true;
+    }
+    void tint_field(const bool newstep = true);
+
+    inline real get_ur() const { return factor; }
+    inline void set_ur(const real factnew) {
+      factor = factnew;
+      boil::oout<<"TIFmodel: Underrelaxation factor = "<<factnew<<boil::endl;
+    }
+
+    inline real tref() const { return tr; }
+    inline void set_tref(const real tnew) {
+      tr = tnew;
+      boil::oout<<"TIFmodel: Tref = "<<tnew<<boil::endl;
+    }
+
+    Scalar tif, tifold;
+
+    real Tint(const int dir, const Comp &mcomp, const real frac,
+              const int i, const int j, const int k) const;
+    real Tint_old(const int dir, const Comp &mcomp, const real frac,
+              const int i, const int j, const int k) const;
+    real Tint(const int i, const int j, const int k) const;
+    real Tint_old(const int i, const int j, const int k) const; 
+
+    void set_weak_limiting(const real tmin, const real tmax);
+
+    static real calculate_heat_transfer_resistance(
+                                          const real tr, const real rhov,
+                                          const real mmass, const real latent,
+                                          const real accommodation);
+
+  protected:
+    real factor; /* under-relaxation factor */
+    real tmin, tmax;
+    bool weaklim;
+
+    bool store_tif,variable_tif;
+    real tr;
+
+    Topology * topo;
+    ScalarInt iflag, tempflag, tempflag2;
+    Scalar stmp;
+
+    virtual void model() {};
+    void extend_tint();
+
+    inline real underrelaxation(const real tintnew, const real tifold);
+};
+
+#endif
---
+++ Src/Equation/Tifmodel/tif_tint_field.cpp
@@ -0,0 +1,62 @@
+#include "tif.h"
+
+inline real TIF::underrelaxation(const real tintnew, const real tifold) {
+  return  tintnew*factor + (tifold) * (1.0-factor);
+}
+
+/***************************************************************************//**
+ *  Calculates interface temperature field
+ *  Tn+1 = Tint*factor + Tn*(1-factor)
+*******************************************************************************/
+void TIF::tint_field(const bool newstep) {
+  if(variable_tif) {
+    if(store_tif) {
+      if(newstep) {
+        tifold = tif;
+      }
+    }
+
+    /* individual tifmodel */
+    model();
+
+    if(weaklim) {
+      for_vijk(tif,i,j,k) {
+        if(topo->interface(i,j,k)) {
+          tif[i][j][k] = std::min(tmax,
+                                  std::max(tmin,tif[i][j][k])
+                                  );
+        }
+      }
+    }
+
+    tif.bnd_update();
+    tif.exchange_all();
+
+    /* under-relaxation */
+    if(store_tif) {
+      if(factor < 1.00) {
+        for_vijk(tif,i,j,k) {
+          if(topo->interface(i,j,k)) {
+            real tintnew = tif[i][j][k];
+            tif[i][j][k] = underrelaxation(tintnew,tifold[i][j][k]);
+          }
+        }
+      }
+    } 
+   
+    tif.bnd_update();
+    tif.exchange_all();
+
+    /* extend the field */
+    extend_tint();
+  
+    if(!store_tif) {
+      tifold = tif;
+      boil::oout<<"TIFmodel::tint_field()  initialize tif"<<"\n";
+      store_tif = true;
+    } 
+
+  } /* variable */
+
+  return;
+}
