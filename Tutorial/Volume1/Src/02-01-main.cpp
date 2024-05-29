d82c2a28d216d92391f08408af5c08c94212c6be
sato <yohei.sato@psi.ch>
2024-05-29 15:27:13 +0200

    delete duplicated folder

--- Tutorial/Volume1/Src/02-01-main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Include/psi-boil.h"
-
-/******************************************************************************/
-main(int argc, char * argv[]) {
-
-  boil::timer.start();
-
-  boil::timer.stop();
-}
