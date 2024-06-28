42c74f47bef01d28ed5b80d64e98d270e77405a8
sato <yohei.sato@psi.ch>
2024-06-28 15:31:40 +0200

    modify comment

--- Src/Field/Scalar/scalar_rm.cpp
+++ Src/Field/Scalar/scalar_rm.cpp
@@ -6,7 +6,7 @@ void Scalar::rm(const char * nm, const int it) {
   /* file name */
   std::string name = name_file(nm, ".bck", it, boil::cart.iam());
 
-  /* open a file */
+  /* remove a file */
   remove(name.c_str());
   
 }
