452969a48e56ffbf26e32d9d1fc3067d14722737
sato <yohei.sato@psi.ch>
2025-04-11 11:14:30 +0200

    debug: norm_cc_kmin -> norm_cc_jmin

--- Src/Equation/Centered/VOF/vof_norm_cc_near_bnd.cpp
+++ Src/Equation/Centered/VOF/vof_norm_cc_near_bnd.cpp
@@ -83,7 +83,7 @@ void VOF::norm_cc_near_bnd(const Scalar & val) {
 
     // south
     if (dom->ibody().off(i,j-1,k)) {
-      norm_cc_kmin(val,i,j,k);
+      norm_cc_jmin(val,i,j,k);
     }
 
     // north
