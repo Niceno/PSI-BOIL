7c9116a3b6a314886dadd720a2d1b416dd05342f
Akira-Imazu <imazu.akira551@mail.kyutech.jp>
2025-10-29 18:41:24 +0900

    update heat flux calculation near wall.

--- Src/Equation/Centered/EnthalpyFD/enthalpyfd.cpp
+++ Src/Equation/Centered/EnthalpyFD/enthalpyfd.cpp
@@ -12,6 +12,7 @@ EnthalpyFD::EnthalpyFD(const Scalar & PHI,
                        Linear * S,
                        Matter * f,
                        const real Tsat,
+		       const real ri,
                        Matter * s) :
 /*---------------------+ 
 |  initialize parent   |
@@ -21,6 +22,7 @@ EnthalpyFD::EnthalpyFD(const Scalar & PHI,
   iflag (  *C  .domain())
 {
   tsat = Tsat,
+  Ri = ri;
   rhol = fluid()->rho(1),
   rhov = fluid()->rho(0),
   cpl  = fluid()->cp(1),
@@ -38,6 +40,8 @@ EnthalpyFD::EnthalpyFD(const Scalar & PHI,
   turbP=0.9;
   laminar=true;
   conv_divu_subtract=true;
+  heater_power=50000;
+  half_heater=5.0e-3;
 
   phi.bnd_update();
 
